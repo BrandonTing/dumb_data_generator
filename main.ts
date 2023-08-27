889a210840436f5a6383aea8d379326f3ccfee9d
Brandon Ting <brandon1221t@gmail.com>
2023-08-27 11:18:58 +0800

    init mapping

--- main.ts
+++ main.ts
@@ -1,3 +1,47 @@
-import { string } from "./src/schema/mod.ts";
+import { SerializedSchema, genZodSchema } from "./src/schema/mod.ts";
+import { generateMock } from '@anatine/zod-mock';
 
-console.log(string.parse('sfdsfsf'))
+// expected output 
+const input = [
+    {
+        name: "userId",
+        type: "number",
+    },
+    {
+        name: "name",
+        type: "string",
+    },
+    // {
+    //     name: "followers",
+    //     type: "array",
+    //     children: "string"
+    // },
+    // {
+    //     name: "posts",
+    //     type: "array",
+    //     children: "object"
+    // },
+    // {
+    //     name: "post",
+    //     type: "object",
+    //     parent: "posts"
+    // },
+    // {
+    //     name: "name",
+    //     type: "string",
+    //     parent: "post"
+    // },
+    // {
+    //     name: "content",
+    //     type: "string",
+    //     parent: "post"  
+    // },
+    {
+        name: "isActive",
+        type: "boolean",
+    },
+] satisfies SerializedSchema;
+
+const inputSchema = genZodSchema(input);
+console.log(generateMock(inputSchema))
+console.log(generateMock(inputSchema))
--- src/schema/mod.ts
+++ src/schema/mod.ts
@@ -1,3 +1,25 @@
 import { z } from "zod";
 
 export const string = z.string();
+export const number = z.number();
+export const boolean = z.boolean();
+
+export const ZodMap = {
+    string,
+    number,
+    boolean
+} as const
+
+export type SerializedSchema = Array<{
+    name: string,
+    type: keyof typeof ZodMap, 
+}>
+
+export const genZodSchema = (serializedSchema: SerializedSchema): z.Schema => {
+    const objectBodySchema: {
+        [key: string]: z.Schema
+    } = serializedSchema.reduce((pre, cur) => {
+        return Object.assign(pre, {[cur.name]:ZodMap[cur.type] })
+    }, {}) 
+    return z.object(objectBodySchema)
+}
